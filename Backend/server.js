056a2f9357c881530dd7f8229bd72bd0d850aa5e
dudaslevente <dudaslevente@turr.hu>
2024-10-16 08:53:30 +0200

    Backend létrehozása

---
+++ Backend/server.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require('express');
+var ejs = require('ejs');
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : process.env.DBHOST,
+  user     : process.env.DBUSER,
+  password : process.env.DBPASS,
+  database : process.env.DBNAME
+});
+
+connection.connect((err) => {
+    if (err){
+        console.log(err);
+        return;
+    }
+    console.log(`Connected to database!`);
+});
+
+app.listen(port, ()=>{
+    console.log(`Server listening on port ${port}...`);
+});
